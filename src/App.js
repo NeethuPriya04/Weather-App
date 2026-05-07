f10713b0a7db32e297b906c473b549bd86298044
NeethuPriya <padamatineethupriya@gmail.com>
2026-05-07 20:40:40 +0530

    Updated weather app UI and features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const API_KEY = "6cd0e3d34a5333774f955e6a307c9f07"; 
+
+  const fetchWeather = async () => {
+    if (!city) return;
+
+    try {
+      setLoading(true);
+      setError("");
+
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      if (!data.main) {
+        setError("City not found");
+        setWeather(null);
+      } else {
+        setWeather(data);
+      }
+    } catch {
+      setError("Something went wrong");
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "app dark" : "app"}>
+      <h1>🌦️ Weather App</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") fetchWeather();
+          }}
+        />
+
+        <button onClick={fetchWeather}>Search</button>
+
+        <button onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? "Light Mode ☀️" : "Dark Mode 🌙"}
+        </button>
+      </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <p className="error">{error}</p>}
+
+      {weather && (
+        <div className="weather-card">
+          <h2>{weather.name}</h2>
+          <p>🌡️ Temp: {weather.main.temp}°C</p>
+          <p>🌥️ Condition: {weather.weather[0].main}</p>
+          <p>💧 Humidity: {weather.main.humidity}%</p>
+          <p>🌬️ Wind: {weather.wind.speed} m/s</p>
+          <p>🤔 Feels Like: {weather.main.feels_like}°C</p>
+          <p>📍Country: {weather.sys.country}</p>
+          <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt="weather icon"
+        />
+        </div>
+      )}
     </div>
   );
 }
